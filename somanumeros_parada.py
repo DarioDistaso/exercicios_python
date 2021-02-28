dd51ac86a8105c7655855fa9e75d6c63e1c9f519
DarioDistaso <dariodistaso@homtail.com>
2021-02-28 17:24:17 -0300

    novo arquivo adicionado

---
+++ somanumeros_parada.py
@@ -0,0 +1,11 @@
+numero = 0
+soma = 0
+cont = 0
+
+while numero != 999:
+    numero = int(input("\nDigite um numero [999 para parar]: "))
+    if numero != 999:
+        soma += numero
+        cont += 1
+print(f'Você digitou \033[0;32m{cont} números\033[m e a soma entre eles foi {soma}')
+
