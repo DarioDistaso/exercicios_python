9ee81999e44d59cb56b62618ef750de0c2cab4c2
DarioDistaso <dariodistaso@homtail.com>
2021-02-28 22:10:26 -0300

    correcao arquivo menor valor

--- maior_valor.py
+++ maior_valor.py
@@ -1,7 +1,7 @@
 '''Crie uma funcao determinar_o maior_numero que receba dois numeros (inteiros ou
 reais) e retorne o maior valor de ambos os numeros'''
 
-'''def determinar_o_maior_numero(a,b):
+def determinar_o_maior_numero(a,b):
     if a > b:
         return a
     elif a < b:
@@ -12,11 +12,7 @@ reais) e retorne o maior valor de ambos os numeros'''
 a = int(input("Digite um numero: "))
 b = int(input("Digite um numero: "))
 
-print("Resultado: ", determinar_o_maior_numero(a,b))'''
+print("Resultado: ", determinar_o_maior_numero(a,b))
 
 
-num = int(input("Digite um numero para ver a sua tabuada: "))
-for i in range(11):
-    tab = num * i
-    print("-----------")
-    print(f'{num} x {i} = {tab}')
+
