e76f7b10333747232b1d3deafe608f0bd765569f
Dario Distaso <dariodistaso@hotmail.com>
2021-06-16 18:51:33 -0300

    Delete exercicio4_programacao_de_aplicativos.py

--- exercicio4_programacao_de_aplicativos.py
+++
@@ -1,9 +0,0 @@
-# Exercício 4. Crie uma função para desenhar uma linha, usando o caractere '_' 
-# (underline). O tamanho da linha deve ser definido na chamada da função.
-
-tamanho = int(input("\nDefina o tamanho da linha: "))
-
-def linha (t):
-    print("_" * t, "\n")
-
-linha(tamanho)
